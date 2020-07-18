a5114497de9a71bbabf9299d12be199b8c74ce63
Alastair Springgay <alasspri@gmail.com>
2020-07-18 23:01:53 +0100

    End of 2.12

--- Work/report.py
+++ Work/report.py
@@ -38,17 +38,31 @@ def read_prices(filename):
     return prices
 
 
+def make_report(stocks, prices):
+    rows = []
+
+    for stock in stocks:
+        rows.append((stock['name'],
+                     stock['shares'],
+                     prices[stock['name']],
+                     prices[stock['name']] - stock['price']
+                     ))
+
+    return rows
+
+
 portfolio = read_portfolio('Data/portfolio.csv')
 prices = read_prices('Data/prices.csv')
+report = make_report(portfolio, prices)
+headers = ('Name', 'Shares', 'Price', 'Change')
 
-# Find current value and purchase value
-current_value = 0
-purchase_value = 0
+print(f"{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}")
+print("-" * 10, end=' ')
+print("-" * 10, end=' ')
+print("-" * 10, end=' ')
+print("-" * 10,)
 
-for stock in portfolio:
-    purchase_value += stock['shares'] * stock['price']
-    current_value += stock['shares'] * prices[stock['name']]
+for name, shares, price, change in report:
+    format_price = f"${price:.2f}"
+    print(f"{name:>10s} {shares:>10d} {format_price:>10s} {change:10.2f}")
 
-print(f"Current Value: {current_value:.2f}")
-print(f"Purchase Value: {purchase_value:.2f}")
-print(f"Gain/loss: {current_value - purchase_value:.2f}")
