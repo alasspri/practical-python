c58b08fbd24912f394557efcf568c537479dbabc
Alastair Springgay <alasspri@gmail.com>
2020-07-18 22:43:36 +0100

    End of 2.8

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,16 +1,33 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
 
-value = 0
 
-f = open('Data/portfolio.csv', 'rt')
-next(f)
+def portfolio_cost(filename):
+    value = 0
+    f = open(filename, 'rt')
+    rows = csv.reader(f)
+    next(rows)
 
-for row in f:
-    rowdata = row.strip().split(',')
-    value += int(rowdata[1]) * float(rowdata[2])
+    for row in rows:
+        try:
+            nshares = int(row[1])
+            shareprice = float(row[2])
+            value += nshares * shareprice
+        except ValueError:
+            print('Bad data, skipping line')
 
-f.close()
+    f.close()
 
-print(f"Total cost {value}")
+    return value
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print(f"Total cost {cost}")
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,54 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+import sys
+from pprint import pprint
+
+
+def read_portfolio(filename):
+    """
+    Reads a portfolio from a file
+    """
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = {'name': row[0],
+                       'shares': int(row[1]),
+                       'price': float(row[2])}
+            portfolio.append(holding)
+    return portfolio
+
+
+def read_prices(filename):
+    """
+    Reads stock prices from a file
+    """
+    prices = {}
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            if row:
+                prices[row[0]] = float(row[1])
+    return prices
+
+
+portfolio = read_portfolio('Data/portfolio.csv')
+prices = read_prices('Data/prices.csv')
+
+# Find current value and purchase value
+current_value = 0
+purchase_value = 0
+
+for stock in portfolio:
+    purchase_value += stock['shares'] * stock['price']
+    current_value += stock['shares'] * prices[stock['name']]
+
+print(f"Current Value: {current_value:.2f}")
+print(f"Purchase Value: {purchase_value:.2f}")
+print(f"Gain/loss: {current_value - purchase_value:.2f}")
